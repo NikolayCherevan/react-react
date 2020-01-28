2fc7322ece5aca03bf76f467a58f2353abc00265
Nickolay Cherevan <cherevan.n.s@gmail.com>
2020-01-28 17:23:28 +0200

    Updates

---
+++ precache-manifest.d9b603d5fbb8801861c58cf5ef69a29b.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199115c6cb1d58746f05af355a221145",
+    "url": "/react-react/index.html"
+  },
+  {
+    "revision": "881e0cab1a7584b924d6",
+    "url": "/react-react/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "bc7aa54c5e5fe1bfcc53",
+    "url": "/react-react/static/css/main.0fe044f1.chunk.css"
+  },
+  {
+    "revision": "881e0cab1a7584b924d6",
+    "url": "/react-react/static/js/2.9ff62ccc.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-react/static/js/2.9ff62ccc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc7aa54c5e5fe1bfcc53",
+    "url": "/react-react/static/js/main.35112fe9.chunk.js"
+  },
+  {
+    "revision": "870861fbce366f2ff289",
+    "url": "/react-react/static/js/runtime-main.ec14b3d2.js"
+  },
+  {
+    "revision": "2dcf2324d8c5365339b70433bcd952e4",
+    "url": "/react-react/static/media/AvenirNextCyr-Bold.2dcf2324.woff"
+  },
+  {
+    "revision": "cb5f2e91d0edea79307ffa674c219a1d",
+    "url": "/react-react/static/media/AvenirNextCyr-Bold.cb5f2e91.ttf"
+  },
+  {
+    "revision": "cd1b4c6212d736229312a937412eedea",
+    "url": "/react-react/static/media/AvenirNextCyr-Bold.cd1b4c62.eot"
+  },
+  {
+    "revision": "348ecb6dc6cac231aaaa725321aea3a0",
+    "url": "/react-react/static/media/AvenirNextCyr-BoldItalic.348ecb6d.woff"
+  },
+  {
+    "revision": "6f558bba0e1dedc912497c80d4fe2228",
+    "url": "/react-react/static/media/AvenirNextCyr-BoldItalic.6f558bba.ttf"
+  },
+  {
+    "revision": "cdbdf32b3d9d478a11653f0bb9872a6b",
+    "url": "/react-react/static/media/AvenirNextCyr-BoldItalic.cdbdf32b.eot"
+  },
+  {
+    "revision": "104be5f79e3ef6239d62bd897fde8d91",
+    "url": "/react-react/static/media/AvenirNextCyr-Demi.104be5f7.ttf"
+  },
+  {
+    "revision": "96adc4bebb865e2bc3c135105c2ffe66",
+    "url": "/react-react/static/media/AvenirNextCyr-Demi.96adc4be.eot"
+  },
+  {
+    "revision": "fe6d7419dcc1b01901050f02303ea003",
+    "url": "/react-react/static/media/AvenirNextCyr-Demi.fe6d7419.woff"
+  },
+  {
+    "revision": "0a25bfd770c0f66c09139d5cc7b4daeb",
+    "url": "/react-react/static/media/AvenirNextCyr-DemiItalic.0a25bfd7.eot"
+  },
+  {
+    "revision": "405ae9542dcd314ff574920cf4c39114",
+    "url": "/react-react/static/media/AvenirNextCyr-DemiItalic.405ae954.woff"
+  },
+  {
+    "revision": "5279d5a5c82ae79515ecaf462d0357c1",
+    "url": "/react-react/static/media/AvenirNextCyr-DemiItalic.5279d5a5.ttf"
+  },
+  {
+    "revision": "639aa9c29711f3129ec5d80d901e22ab",
+    "url": "/react-react/static/media/AvenirNextCyr-Heavy.639aa9c2.woff"
+  },
+  {
+    "revision": "b14619f5cc83d46590b825555375cd36",
+    "url": "/react-react/static/media/AvenirNextCyr-Heavy.b14619f5.ttf"
+  },
+  {
+    "revision": "c7c48f65258ec9fa781d5bd63325e191",
+    "url": "/react-react/static/media/AvenirNextCyr-Heavy.c7c48f65.eot"
+  },
+  {
+    "revision": "2ec1507802262ee3d4d8b8cac1507c67",
+    "url": "/react-react/static/media/AvenirNextCyr-HeavyItalic.2ec15078.woff"
+  },
+  {
+    "revision": "c00d51e26a2c231ac479ede7788cd888",
+    "url": "/react-react/static/media/AvenirNextCyr-HeavyItalic.c00d51e2.eot"
+  },
+  {
+    "revision": "e962563da9bae7e5165ee4a0f61d9aee",
+    "url": "/react-react/static/media/AvenirNextCyr-HeavyItalic.e962563d.ttf"
+  },
+  {
+    "revision": "3e31fb5719598f7d79c0eaa003991a62",
+    "url": "/react-react/static/media/AvenirNextCyr-Italic.3e31fb57.woff"
+  },
+  {
+    "revision": "51972d571dc4fbd6faff884ffa007c29",
+    "url": "/react-react/static/media/AvenirNextCyr-Italic.51972d57.ttf"
+  },
+  {
+    "revision": "5de772963e18f14966e61d2bae8bfda4",
+    "url": "/react-react/static/media/AvenirNextCyr-Italic.5de77296.eot"
+  },
+  {
+    "revision": "2bda12c6f0fc7cdcf3f70b3225e39d52",
+    "url": "/react-react/static/media/AvenirNextCyr-Light.2bda12c6.woff"
+  },
+  {
+    "revision": "c91f454865c62e8e068ce224a49fccc9",
+    "url": "/react-react/static/media/AvenirNextCyr-Light.c91f4548.ttf"
+  },
+  {
+    "revision": "e343ba2c0a81c8ea4aea9bbb0f2ddb0f",
+    "url": "/react-react/static/media/AvenirNextCyr-Light.e343ba2c.eot"
+  },
+  {
+    "revision": "0ebbb046ddc2704add68c609f8c5fd3e",
+    "url": "/react-react/static/media/AvenirNextCyr-LightItalic.0ebbb046.woff"
+  },
+  {
+    "revision": "64954fb75330e520cab5684ace47da79",
+    "url": "/react-react/static/media/AvenirNextCyr-LightItalic.64954fb7.ttf"
+  },
+  {
+    "revision": "95939d3fba08e42132eb028cc2970997",
+    "url": "/react-react/static/media/AvenirNextCyr-LightItalic.95939d3f.eot"
+  },
+  {
+    "revision": "241293dd350f84881201063e58f13109",
+    "url": "/react-react/static/media/AvenirNextCyr-Medium.241293dd.woff"
+  },
+  {
+    "revision": "576011fe6e1aa00766f8521305d6602b",
+    "url": "/react-react/static/media/AvenirNextCyr-Medium.576011fe.eot"
+  },
+  {
+    "revision": "862c7c4267856b43beabe738a13c281e",
+    "url": "/react-react/static/media/AvenirNextCyr-Medium.862c7c42.ttf"
+  },
+  {
+    "revision": "063f62c17593b8d4080993e85c6c1d8a",
+    "url": "/react-react/static/media/AvenirNextCyr-MediumItalic.063f62c1.eot"
+  },
+  {
+    "revision": "33b8aaeaa36918de0830703cb530898e",
+    "url": "/react-react/static/media/AvenirNextCyr-MediumItalic.33b8aaea.woff"
+  },
+  {
+    "revision": "f2fc32fb724bca617be17961d4a846d2",
+    "url": "/react-react/static/media/AvenirNextCyr-MediumItalic.f2fc32fb.ttf"
+  },
+  {
+    "revision": "4171ed3ba504b1da3b601a6b52e94dce",
+    "url": "/react-react/static/media/AvenirNextCyr-Regular.4171ed3b.eot"
+  },
+  {
+    "revision": "97b615b907fd3510f9129eac4a731f6f",
+    "url": "/react-react/static/media/AvenirNextCyr-Regular.97b615b9.ttf"
+  },
+  {
+    "revision": "a81229c89f968c0b8eb6544e2a90d17a",
+    "url": "/react-react/static/media/AvenirNextCyr-Regular.a81229c8.woff"
+  },
+  {
+    "revision": "a20377e2e35c0c9b514194619665defc",
+    "url": "/react-react/static/media/AvenirNextCyr-ThinItalic.a20377e2.woff"
+  },
+  {
+    "revision": "afd4120fe4125d1725b9ca65b7ef78fc",
+    "url": "/react-react/static/media/AvenirNextCyr-ThinItalic.afd4120f.eot"
+  },
+  {
+    "revision": "bb97e3a1dc8431300de8af6b4862dda2",
+    "url": "/react-react/static/media/AvenirNextCyr-ThinItalic.bb97e3a1.ttf"
+  },
+  {
+    "revision": "1a44681e3536dc4ddee2196d96ae2169",
+    "url": "/react-react/static/media/AvenirNextCyr-UltraLight.1a44681e.ttf"
+  },
+  {
+    "revision": "2b9960b9669b9f65f78df5c83b9c40c9",
+    "url": "/react-react/static/media/AvenirNextCyr-UltraLight.2b9960b9.eot"
+  },
+  {
+    "revision": "decf8516e2fff3aa12211d6b4cb18e42",
+    "url": "/react-react/static/media/AvenirNextCyr-UltraLight.decf8516.woff"
+  },
+  {
+    "revision": "3a8ca6942a3980fac50e74cacaed6862",
+    "url": "/react-react/static/media/AvenirNextCyr-UltraLightIt.3a8ca694.eot"
+  },
+  {
+    "revision": "533232314791701727b240ed84c3628c",
+    "url": "/react-react/static/media/AvenirNextCyr-UltraLightIt.53323231.woff"
+  },
+  {
+    "revision": "e2651b88579ab90cb3be5c8efafe25b6",
+    "url": "/react-react/static/media/AvenirNextCyr-UltraLightIt.e2651b88.ttf"
+  },
+  {
+    "revision": "3e2a2b428492588dcd141c6367854e86",
+    "url": "/react-react/static/media/icomoon.3e2a2b42.woff"
+  },
+  {
+    "revision": "4c16a2970d56b9b59cb031869ed1ccc8",
+    "url": "/react-react/static/media/icomoon.4c16a297.ttf"
+  },
+  {
+    "revision": "6b7fd3e74658bf8efc73bb8ff9a5d48c",
+    "url": "/react-react/static/media/icomoon.6b7fd3e7.svg"
+  },
+  {
+    "revision": "89eb06fd89b3db76a3d5d12904d24712",
+    "url": "/react-react/static/media/icomoon.89eb06fd.eot"
+  }
+]);
