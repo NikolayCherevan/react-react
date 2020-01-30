ebe3f63cd305b75a9a6c64309f95039b1b8b6749
Nickolay Cherevan <cherevan.n.s@gmail.com>
2020-01-30 17:12:33 +0200

    ok

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Avatar } from "antd";
-import Moment from 'react-moment';
+import Moment from "react-moment";
+import EventsList from "./EventsList"
+import img from './../img/background-events.png'
 const Events = () => {
-    let newDate = new Date();
-    const format = 'DD . MM';
-    const dateToFormat = 'DD . MM';
+  let newDate = new Date();
+  const format = "DD . MM";
+  const dateToFormat = "DD . MM";
   return (
     <section>
       <div className="wrapper-event">
         <div className="card-wrapper">
-          <div className="card-header">
-            <div className="avatar-name-wrapp">
-              {" "}
-              <Avatar shape="square" size="small" icon="user" />{" "}
-              <span className="username"> Jessica </span>
-            </div>{" "}
-            <div className="date-wrapper"> <span className="data">   <Moment> {dateToFormat}</Moment></span> <span className="data2">   <Moment>{dateToFormat}</Moment></span></div>
-           
-          </div>
-          <div className='event-desc'> Joined dsgn.work Platform </div> 
-          <div className="card-boody"> </div>
+        
+          <div className="card-boody">
+            <EventsList />
+         <div className='figure-wrapp'> <img src={img} alt='plans' />
+         <span className='subtitle-event'> You’re all set!</span>
+         
+         </div>
+             </div>
         </div>
       </div>
     </section>
---
+++ src/Components/EventsList.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Avatar } from "antd";
+import Moment from "react-moment";
+
+const EventsList = () => {
+  let newDate = new Date();
+  const format = "DD . MM";
+  const dateToFormat = "DD . MM";
+  let events = [
+      'Start working',
+      'Drop project',
+      'Leave copmpany',
+      'Start working',
+      'Drop project',
+      'Leave copmpany',
+      'Start working',
+      'Drop project',
+      'Leave copmpany',
+      'Start working',
+      'Drop project',
+      'Leave copmpany'
+  ]
+
+
+    let listItems = events.map((event, i) => {
+        return (
+            <section key= {i}>
+            <div className="wrapper-events">
+              <div className="avatar-name-wrapp">
+                {" "}
+                <Avatar shape="square" size="small" icon="user" />{" "}
+                <span className="username"> Jessica </span>
+              </div>{" "}
+              <div className="date-wrapper">
+                {" "}
+                <span className="data">
+                  {" "}
+                  <Moment> </Moment>
+                </span>{" "}
+                <span className="data2">
+                  {" "}
+                  <Moment>}</Moment>
+                </span>
+              </div>
+            </div>
+            <div className="event-desc"> {event}</div>
+          </section>
+        )
+    }
+
+ 
+  );
+
+       
+  
+  
+ 
+     
+     
+
+  return (
+      <div> {listItems}</div>
+  
+  );
+};
+export default EventsList;
